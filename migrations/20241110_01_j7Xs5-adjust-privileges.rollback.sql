77c3de50b8a4db3105780dd04d97d9b320ac6302
Vladislav Perlin <vaperlin@yandex.ru>
2024-11-10 19:33:28 +0300

    Adjusted privileges

--- migrations/20241110_01_j7Xs5-adjust-privileges.rollback.sql
+++ migrations/20241110_01_j7Xs5-adjust-privileges.rollback.sql
@@ -3,3 +3,7 @@
 
 revoke select, insert, update on subgroups from public ;
 revoke usage on sequence subgroups_iid_seq from public ;
+
+
+revoke select, insert, delete on subgroups_students from public ;
+revoke select, insert, delete on subgroups_subjects from public ;
--- migrations/20241110_01_j7Xs5-adjust-privileges.sql
+++ migrations/20241110_01_j7Xs5-adjust-privileges.sql
@@ -3,3 +3,6 @@
 
 grant select, insert, update on subgroups to public ;
 grant usage on sequence subgroups_iid_seq to public ;
+
+grant select, insert, delete on subgroups_students to public ;
+grant select, insert, delete on subgroups_subjects to public ;
