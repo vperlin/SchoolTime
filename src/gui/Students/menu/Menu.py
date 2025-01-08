2cf7170aeb47651089bc6a4ccfd3c48ec4d18135
Vladislav Perlin <vaperlin@gmail.com>
2025-01-08 19:29:23 +0300

    T

--- src/gui/Students/menu/Menu.py
+++ src/gui/Students/menu/Menu.py
@@ -7,3 +7,8 @@ class Menu(QMenu):
         super().__init__(parent)
 
         self.setTitle(self.tr('Students'))
+
+        self.__act_add_subgroup = self.addAction(self.tr('Add subgroup'))
+        self.__act_rename_subgroup = self.addAction(self.tr('Rename subgroup'))
+        self.__act_attach_subject = self.addAction(self.tr('Attach subject'))
+        self.__act_detach_subject = self.addAction(self.tr('Detach subject'))
